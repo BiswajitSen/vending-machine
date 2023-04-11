af46a30b5ed2f2f2524fb45cff1bd788903cd68c
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2023-04-11 18:15:24 +0530

    Added functionality to count optimum number of coins from a given set.

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,36 +1,14 @@
-const dispence2rsCoins = function(rupees) {
-  return rupees % 2;
+const dispenceCoins = function(rupees, value) {
+  return Math.floor(rupees / value);
 }
 
-const intDivByTwo = function(rupees) {
-  if(rupees < 2) return 0;
-  return Math.floor(rupees / 2);
-}
-
-const intDivByFive = function(rupees) {
-  if(rupees < 5 ) return 0;
-  return Math.floor(rupees / 5);
-}
-
-const intDivByTen = function(rupees) {
-  if(rupees < 10 ) return 0;
-  return Math.floor(rupees / 10);
-}
-const coinOfOne = function(rupees) {
-  return rupees;
-}
-const getCoinsCount = function(rupees) {
+const getCoinsCount = function(rupees, currencies) {
   let count = 0;
   let totalMoney = rupees;
-
-  count += intDivByTen(totalMoney);
-  totalMoney = totalMoney % 10;
-
-  count += intDivByFive(totalMoney);
-  totalMoney = totalMoney % 5;
-
-  count += intDivByTwo(totalMoney);
-  totalMoney = totalMoney % 2;
+  for ( let i = currencies.length - 1; i >= 0; i--) {
+    count += dispenceCoins(totalMoney, currencies[i]);
+    totalMoney %= currencies[i];
+  }
 
   return count += totalMoney;
 
