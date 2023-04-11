206f42724a62419e5a9a73c9f9060dc2906bdcd6
Biswajit Sen <biswajitsen@Biswajits-MacBook-Pro.local>
2023-04-11 17:19:20 +0530

    added functionality for minimal coin count includs 1, 2, 5, 10 rs. coins

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,8 +2,38 @@ const dispence2rsCoins = function(rupees) {
   return rupees % 2;
 }
 
+const intDivByTwo = function(rupees) {
+  if(rupees < 2) return 0;
+  return Math.floor(rupees / 2);
+}
+
+const intDivByFive = function(rupees) {
+  if(rupees < 5 ) return 0;
+  return Math.floor(rupees / 5);
+}
+
+const intDivByTen = function(rupees) {
+  if(rupees < 10 ) return 0;
+  return Math.floor(rupees / 10);
+}
+const coinOfOne = function(rupees) {
+  return rupees;
+}
 const getCoinsCount = function(rupees) {
-  return (dispence2rsCoins(rupees)) + (rupees - dispence2rsCoins(rupees) * 2);
+  let count = 0;
+  let totalMoney = rupees;
+
+  count += intDivByTen(totalMoney);
+  totalMoney = totalMoney % 10;
+
+  count += intDivByFive(totalMoney);
+  totalMoney = totalMoney % 5;
+
+  count += intDivByTwo(totalMoney);
+  totalMoney = totalMoney % 2;
+
+  return count += totalMoney;
+
 }
 
 exports.getCoinsCount = getCoinsCount;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,5 +4,8 @@ const vendingMachine = require("../src/vending-machine.js");
 
 testing.assertTest(1, vendingMachine.getCoinsCount(2), '','for rs. 2 should be 1 coin');
 testing.assertTest(2, vendingMachine.getCoinsCount(3), '','for rs. 3 should be 2 coins');
+testing.assertTest(1, vendingMachine.getCoinsCount(5), '','for rs. 5 should be 1 coins');
+testing.assertTest(2, vendingMachine.getCoinsCount(7), '','for rs. 7 should be 2 coins');
+testing.assertTest(2, vendingMachine.getCoinsCount(20), '','for rs. 20 should be 2 coins');
 console.log(testing.groupLogByKey('result'));
 
