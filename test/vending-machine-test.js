93a453693df0747e79ef4989c9169131671ae342
Biswajit Sen <BiswajitSen@users.noreply.github.com>
2023-04-12 18:18:45 +0530

    added new test case as requirement

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -7,6 +7,7 @@ const test = function() {
   testing.assertTest(1, vendingMachine.dispenseCoins(5, [1, 2, 5]), '','for rs. 5 should be 1 coins');
   testing.assertTest(2, vendingMachine.dispenseCoins(7, [1, 2, 5]), '','for rs. 7 should be 2 coins');
   testing.assertTest(2, vendingMachine.dispenseCoins(20, [1, 2, 5, 10]), '','for rs. 20 should be 2 coins');
+  testing.assertTest(4, vendingMachine.dispenseCoins(13, [1, 4, 7]), '','for rs. 20 should be 2 coins');
 
   testing.getFailedCases();
   testing.displaySummary();
