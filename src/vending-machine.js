f7115b7757781faa4a8107addcbfc211e39b2f73
Biswajit Sen <BiswajitSen@users.noreply.github.com>
2023-04-13 13:28:20 +0530

    Implmented bubble sorts

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,28 +2,19 @@ const getQuotient = function(divisor, dividend) {
   return Math.floor(divisor / dividend);
 }
 
-const findMin = function(numbers){
-  let min = numbers[0];
+const bubbleSort = function(list){
+  const numbers = list.slice(0);
 
-  for(let index = 0; index <  numbers.length; index++) {
-    min = Math.min(min, numbers[index]);
+  for(let i = 0; i < numbers.length - 1; i++){
+    for(let j = 0; j < numbers.length - i - 1; j++){
+      if(numbers[j] < numbers[j + 1]){
+        const temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+      }
+    }
   }
-
-  return min;
-}
-
-const minSort = function(numbers) {
-  let numberSet = numbers.slice(0);
-  const sortedSet = [];
-
-  for(let i = 0; i < numberSet.length; i++){
-    const currentMin = findMin(numberSet);
-    const currentMinIndex = numberSet.indexOf(currentMin);
-
-    sortedSet.unshift(currentMin);
-    numberSet[currentMinIndex] = Infinity;
-  }
-  return sortedSet;
+  return numbers;
 }
 
 const dispenseCoins = function(rupees, currencies) {
@@ -31,7 +22,7 @@ const dispenseCoins = function(rupees, currencies) {
   let totalMoney = rupees;
   let requiresCurrencies = currencies.slice(0);
 
-  requiresCurrencies = minSort(requiresCurrencies);
+  requiresCurrencies = bubbleSort(requiresCurrencies);
 
   for (const current of requiresCurrencies) {
     count += getQuotient(totalMoney, current);
@@ -43,5 +34,5 @@ const dispenseCoins = function(rupees, currencies) {
 }
 
 exports.dispenseCoins = dispenseCoins;
-exports.minSort = minSort;
-exports.findMin = findMin;
+exports.bubbleSort = bubbleSort;
+;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -12,9 +12,8 @@ const test = function() {
   testing.assertTest(4, vendingMachine.dispenseCoins(13, [7, 4, 1]), 'For amount 13, with denominations [7, 4, 1], 4 coins should be vend');
   testing.assertTest(4, vendingMachine.dispenseCoins(13, [1, 7, 4]), 'For amount 13, with denominations [1, 7, 4], 4 coins should be vend');
 
-  testing.assertArray([42, 7, 1], vendingMachine.minSort([1, 42, 7]), '[1 ,42 ,7] should be sorted to [42, 7, 1]' );
-  testing.assertArray([42], vendingMachine.minSort([42]), 'Sorting array of one element should return the same array.');
-  testing.assertTest(1, vendingMachine.findMin([7, 3, 1]), 'In the set [7, 3, 1], 1 should be the minimum value');
+  testing.assertArray([42, 7, 1], vendingMachine.bubbleSort([1, 42, 7]), '[1 ,42 ,7] should be sorted to [42, 7, 1]' );
+  testing.assertArray([42], vendingMachine.bubbleSort([42]), 'Sorting array of one element should return the same array.');
 
   testing.displaySummary();
 }
